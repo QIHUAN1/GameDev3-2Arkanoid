310decbcac142ca8f39723fe7f2b42f3d874b44d
QIHUAN1 <qjiang1@inside.artcenter.edu>
2023-03-21 02:34:08 -0700

    1

--- Assets/Script/ThePlatform.cs
+++ Assets/Script/ThePlatform.cs
@@ -5,20 +5,44 @@ using UnityEngine;
 public class ThePlatform : MonoBehaviour
 {
     private Camera mainCamera;
-    private float thePlat;
     
+    private float thePlatY;
+    private float mousePosX;
+
+    [SerializeField]
+    private float platMoveMax;
+    [SerializeField]
+    private float platMoveMin;
+
+
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = FindObjectOfType <Camera>();
-        thePlat = this.transform.position.y;
+        thePlatY = this.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePosx = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0 , 0)).x;
-        
-        this.transform.position = new Vector3(mousePosx,thePlat,0);
+
+        mousePosX = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0 , 0)).x;
+        PlatLimit();
+        this.transform.position = new Vector3(mousePosX, thePlatY,0);
+    }
+
+
+    void PlatLimit()
+    {
+        if(mousePosX > platMoveMax)
+        {
+            mousePosX = platMoveMax;
+        }
+
+        if (mousePosX < platMoveMin)
+        {
+            mousePosX = platMoveMin;
+        }
+
     }
 }
