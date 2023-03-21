0c46d94397e0dcd5001d97e566008aa1a78bc7c2
QIHUAN1 <qjiang1@inside.artcenter.edu>
2023-03-21 03:23:03 -0700

    1

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float minY;
+    public float maxVelo = 15f;
+
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(rb.velocity.magnitude > maxVelo)
+        {
+           rb.velocity = Vector3.ClampMagnitude(rb.velocity,maxVelo);
+        }
+
+
+        
+        if(transform.position.y < minY)
+        {
+            ResetBall();
+        }
+    }
+
+    void ResetBall ()
+    {
+        //rb.velocity = Vector3.zero;
+    }
+}
